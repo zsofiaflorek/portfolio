45094902a17193ee66ae14d0d7395606f3710645
Zsofia Florek <58311322+zsofiaflorek@users.noreply.github.com>
2021-07-29 17:22:40 +0200

    Implement landing page design

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,18 @@
 import classNames from "classnames";
+import { Menu } from "./menu";
 
 export function Layout({ children, className }) {
   return (
-    <main
-      className={classNames(
-        "mt-10 mb-16 px-6 md:max-w-[500px] md:mx-auto lg:max-w-[1000px]",
-        className
-      )}
-    >
-      {children}
-    </main>
+    <div>
+      <Menu />
+      <main
+        className={classNames(
+          "mt-10 mb-16 px-6 md:max-w-[500px] md:mx-auto lg:max-w-screen-lg",
+          className
+        )}
+      >
+        {children}
+      </main>
+    </div>
   );
 }
