f46e0b5e2e626f5ff49625cf1c871a41034d8ffa
Zsofia Florek <58311322+zsofiaflorek@users.noreply.github.com>
2021-08-28 12:46:55 +0200

    Fix menu bar overflow

--- components/menu.js
+++ components/menu.js
@@ -3,11 +3,11 @@ import { MenuItem } from "./menuItem";
 export function Menu() {
   return (
     <nav className=" font-semibold uppercase shadow-md border-b-2 border-primary">
-      <div className="max-w-screen-lg flex gap-4 justify-between items-center mx-auto px-6">
-        <h1 className="w-20 flex-shrink-0 text-primary text-md mt-2 sm:w-auto">
+      <div className="max-w-screen-lg mx-auto px-4 pt-4 sm:px-6 sm:pt-0 sm:flex sm:gap-2 sm:justify-between sm:items-center">
+        <h1 className="block text-primary text-base text-center sm:flex sm:flex-shrink-0 sm:text-md">
           Zsofia Florek
         </h1>
-        <ul className="flex gap-2 justify-end overflow-hidden py-4 lg:gap-10">
+        <ul className="flex gap-2 justify-between overflow-hidden py-2 text-sm sm:text-md sm:gap-6 sm:py-4 md:gap-10">
           <MenuItem href="/">Home</MenuItem>
           <MenuItem href="/about">About</MenuItem>
           <MenuItem href="/portfolio">Portfolio</MenuItem>
